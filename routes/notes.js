43c347c533ba9e4274ad387d871f40eaaf52d365
Anna <kayzerscake@gmail.com>
2024-02-15 21:50:43 -0500

    test

--- routes/notes.js
+++ routes/notes.js
@@ -1,16 +1,10 @@
 const fb = require('express').Router();
 
-// Helper function to generate unique ids
-const uuid = require('../helpers/uuid');
-
-// Helper functions for reading and writing to the JSON file
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-
 // GET Route for retrieving all the feedback
 fb.get('/', (req, res) => {
   console.info(`${req.method} request received for feedback`);
 
-  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)));
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for submitting feedback
