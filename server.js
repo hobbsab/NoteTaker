136e47f15dc619ea364799384d0aaa960ba00075
Anna <kayzerscake@gmail.com>
2024-02-15 23:14:20 -0500

    readme

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 // app.use('/api', api);
 app.use(express.static('public'));
 
+// HTML paths
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
@@ -17,6 +18,7 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, './public/notes.html'))
 )
 
+// app listening at the port 3001
 app.listen(port, () =>
   console.log(`App listening at http://localhost:${port}`)
 );
